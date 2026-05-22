4a8f6a7ac1366d7cc88cf14d7b42d2bdfea42542
mohamed8479 <148390783+mohamed8479@users.noreply.github.com>
2026-05-22 21:34:14 +0100

    version nouveau

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fa6d3e154e67a9524f81ca5bf847d897
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "75417cb39244f49b8f6c8bad770ad85d",
+"flutter_bootstrap.js": "dfaf6c8c3b1d45572fd1c28dfd4b05d4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
